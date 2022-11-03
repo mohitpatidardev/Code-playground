288d38a7412b46da31fa892dc679c4db2480913d
mohit patidar <mohitpatidar2186@gmail.com>
2022-11-04 01:20:29 +0530

    modify

--- Java/first.java
+++ Java/first.java
@@ -1,6 +1,7 @@
 
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+public class first {  // public class name should be same as file name
+    public static void main(String args[]) {
+        System.out.println("Hello, World!\n"); // also use ln for line space
+        System.out.print("Hello, World!\n"); 
     }
 }
