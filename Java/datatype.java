54d870d7b1eeb853908a550b3ad5a83b451331c0
mohit patidar <mohitpatidar2186@gmail.com>
2022-11-04 02:15:02 +0530

    modify

---
+++ Java/datatype.java
@@ -0,0 +1,16 @@
+public class datatype {
+    public static void main(String args[]) {
+        byte b = 127;
+        System.out.println(b);
+        char ch = 'A';
+        System.out.println(ch);
+        boolean var = 5>7;
+        System.out.println(var);
+        // float var1  = 10.5;
+        int num =  65;
+        // long
+        // double
+        short n = 240;
+
+}
+}
