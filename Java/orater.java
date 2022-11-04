cc062924350af2cf69322e4d6120aa145b181fbb
mohit patidar <mohitpatidar2186@gmail.com>
2022-11-04 22:58:48 +0530

    modify

---
+++ Java/orater.java
@@ -0,0 +1,14 @@
+public class orater {
+    public static void main(String args[]){
+        int x,y,z;
+
+        x=y=z=2;
+
+        x+=y;
+        y-=z;
+        z/=(x+y);
+
+
+        System.out.println(x+" " + y+" " +z);
+    }
+}
