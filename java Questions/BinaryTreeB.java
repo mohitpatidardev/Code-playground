4550f867adc5fc70f31823ad867a785dfe15d894
mohit patidar <mohitpatidar2186@gmail.com>
2022-11-26 00:35:27 +0530

    modify

--- java Questions/BinaryTreeB.java
+++ java Questions/BinaryTreeB.java
@@ -1,12 +1,12 @@
 
 public class BinaryTreeB {
     static class Node{
-        int date;
+        int data;
         Node left;
         Node right;
 
-        Node(int date){
-            this.date = date;
+        Node(int data){
+            this.data = data;
             this.left = null;
             this.right = null;
         }
@@ -32,18 +32,42 @@ public class BinaryTreeB {
             if(root ==  null){
                 return;
             }
-            System.out.print(root.date+" ");
+            System.out.print(root.data+" ");
             preorder(root.left);
             preorder(root.right);
 
         }
+
+        public static void inorder(Node root){
+            if(root ==  null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data+" ");
+            inorder(root.right);
+
+        }
+
+        public static void postorder(Node root){
+            if(root ==  null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data+" ");
+
+        }
+
+
     }
 
     public static void main(String args[]){
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.date);
-        tree.preorder(root);
+        System.out.println(root.data);
+        // tree.preorder(root);
+        // tree.inorder(root);
+        tree.postorder(root);
     }
 }
