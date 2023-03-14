83da1f1ca20e0f32c808f07a6a3271457b523669
mohit patidar <mohitpatidar2186@gmail.com>
2023-03-14 15:25:43 +0530

    modify

---
+++ Web Development/nodeJS/Custom Modules/mod.js
@@ -0,0 +1,12 @@
+console.log("this is module");
+
+
+function average(arr){
+    let sum = 0;
+    arr.forEach(element => {
+       sum+= element; 
+    });
+    return sum/arr.length;
+}
+
+module.exports = average;
