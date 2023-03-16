f9b1aa1d317cc0162254a7ea808dd0af097c6108
mohit patidar <mohitpatidar2186@gmail.com>
2023-03-16 18:06:43 +0530

    modify

--- nodeJS/express/app.js
+++ nodeJS/express/app.js
@@ -1,35 +1,27 @@
-const express = require("express");
+const express =require("express")
 const path = require("path");
 const app = express();
-const port = 80;
+const port=80;
 
-// for serving static file
-app.use('/static' , express.static('static'))
+app.use('/static', express.static('static'))
 
-// set templat engine as pug
-app.set('viwe engine','pug')
+app.set('view engine','pug')
 
-// set the viwes directory
-app.set('viwe',path.join(__dirname,'viwes'))
+app.set('views', path.join(__dirname,'views'))
 
-// pug demo endpoint 
-
-app.get("/" , (req , res)=>{
-    res.status(500).res.render('demo', { title: 'Hey wewake', message: 'Hello there!' })
+app.get("/demo", (req,res)=>{
+    res.status(200).render('demo', { title: 'Hey', message: 'how to use pug!' })
 });
 
-app.get("/" , (req , res)=>{
-    res.send("this is home page of my first express");
+
+app.get("/", (req,res)=>{
+    res.status(200).send("this is my first express app")
 });
 
-app.get("/about" , (req , res)=>{
-    res.status(500).send("this is about page of my first express");
+app.get("/about", (req,res)=>{
+    res.send("this is my first express app")
 });
 
-app.post("/about" , (req , res)=>{
-    res.send("this is a post request about page of my first express");
-});
-
-app.listen(port , ()=>{
-    console.log(`the application start succesfully ${port}`);
-});
+app.listen(port,()=>{
+    console.log(`the application started successfully on port ${port}`)
+})
