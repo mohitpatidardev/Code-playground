1938a9f01103907d3b8dec176fec2b9efaffced1
mohit patidar <mohitpatidar2186@gmail.com>
2023-03-16 16:49:10 +0530

    modify

--- nodeJS/express/app.js
+++ nodeJS/express/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const port = 80;
+
+// for serving static file
+app.use('/static' , express.static('static'))
+
+// set templat engine as pug
+app.set('viwe engine','pug')
+
+// set the viwes directory
+app.set('viwe',path.join(__dirname,'viwes'))
+
+// pug demo endpoint 
+
+app.get("/" , (req , res)=>{
+    res.status(500).res.render('demo', { title: 'Hey wewake', message: 'Hello there!' })
+});
 
 app.get("/" , (req , res)=>{
     res.send("this is home page of my first express");
