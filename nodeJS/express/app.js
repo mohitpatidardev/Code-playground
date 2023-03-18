ca061b9ec73e1ff1d9c6689ec7812388d6d64832
mohit patidar <mohitpatidar2186@gmail.com>
2023-03-18 20:06:07 +0530

    modify

--- nodeJS/express/app.js
+++ nodeJS/express/app.js
@@ -1,10 +1,12 @@
 const express =require("express")
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port=80;
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
 
 // PUG SPECIFIC STUFF
 app.set('view engine', 'pug') // Set the template engine as pug
@@ -14,6 +16,18 @@ app.set('views', path.join(__dirname, 'views')) // Set the views directory
 app.get('/', (req, res)=>{
     const con = "This is the best content on the internet so far so use it wisely"
     const params = {'title': 'PubG is the best game', "content": con}
+    res.status(200).render('index.pug', params);
+})
+
+app.post('/', (req, res)=>{
+    name = req.body.name
+    age = req.body.age
+    gender = req.body.gender
+    address = req.body.address
+    more = req.body.more
+
+    let outputToWrite = `the name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}`
+    const params = {'message': 'Your form has been submitted successfully'}
     res.status(200).render('index.pug', params);
 })
 
