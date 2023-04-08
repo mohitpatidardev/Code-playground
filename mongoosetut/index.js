5703cc86d9b1bbc83d5d81ac7b64f870fb838685
mohit patidar <mohitpatidar2186@gmail.com>
2023-04-08 13:50:07 +0530

    modify

--- mongoosetut/index.js
+++ mongoosetut/index.js
@@ -1,8 +1,34 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/monty');
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/monty");
 
 var db = mongoose.connection;
-db.on('error' , console.error.bind(console, 'connection error:'));
-db.once('open' , function(){
-    console.log(" we are coected.....");
+db.on("error", console.error.bind(console, "connection error:"));
+
+// db.once("open", function () {
+//   console.log(" we are coected.....");
+// });
+
+const kittySchema = new mongoose.Schema({
+  name: String,
 });
+
+var Kitten = mongoose.model("kitten", kittySchema);
+
+var moonKitty = new Kitten({ name: "moonKitty" });
+console.log(moonKitty.name);
+
+// kittySchema.methods.speak = function () {
+//   var greeting = "my name is " + this.name;
+//   console.log(greeting);
+// };
+
+// moonkitty.save(function (err, moonkitty) {
+//   if (err) return console.error(err);
+//   moonkitty.speak();
+// });
+
+
+// Kitten.find({ name: "moonkitty" },function (err, moonkitty) {
+//   if (err) return console.error(err);
+//   moonkitty.speak();
+// });
