f82fb165b219f3d5f0f1889a9f345c9b216556c0
Mohit Patidar <mohitpatidar2186@gmail.com>
2023-08-11 14:26:04 +0530

    modify

---
+++ java Script practice/index.js
@@ -0,0 +1,25 @@
+// primitive data type --> nn bb ss u
+
+// Number  Null  Boolean Bigint String Symbol Undefined
+
+let a = 435;
+let  b = null;
+let c = true;
+let d = BigInt(344);
+let e = "monty";
+let f = Symbol("s");
+let g 
+
+
+console.log(a,b,c,d,e,f,g);
+console.log(typeof g)
+
+
+// objectes --. non primitive data type 
+
+const item = {
+    name: 'kush',
+    age : '20'
+}
+
+console.log(item["name"]);
---
+++ java Script practice/prac.js
@@ -0,0 +1,23 @@
+let a = "montea"
+let b = 9
+
+console.log(a+b)
+console.log(typeof (a+b))
+
+const obj = {
+    name : 'mohit',
+    age:'20'
+}
+
+obj['learning'] ='java script'
+obj['name'] ='kush'
+console.log(obj)
+
+
+const dictionary = {
+    appreciate : 'to be grateful or thankful' ,
+    apology :'a written or spoken expression of regret',
+}
+
+console.log(dictionary['appreciate'])
+console.log(dictionary.appreciate)
